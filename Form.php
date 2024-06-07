c9ad239734d7e99264b637efcfa76c88fcd8ddd2
joeprogramador4 <josejoelruiz7@gmail.com>
2024-06-07 12:50:07 -0600

    intento

---
+++ Form.php
@@ -0,0 +1,91 @@
+<!DOCTYPE html>
+<html>
+	<head>
+	<meta charset="utf-8">
+	<title>Pagina principal</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+	<link rel="stylesheet" href="css/style.css">
+	<link rel="stylesheet" href="css/animate.css">
+	<link href='https://fonts.googleapis.com/css?family=Roboto:400,100,300,700' rel='stylesheet'>
+	</head>
+	<body>
+    <nav class="navbar navbar-custom navbar-static-top" role="navigation">
+                <div class="container">
+                    <div class="navbar-header pull-left">
+                        <a href="" class="navbar-brand">
+                            <span class="brand-logo"><img src="images/logo.png" alt="" width="300" height="300"/></span>
+                        </a>
+                    </div>
+                    <div class="main-nav pull-right">
+                        <div class="button_container toggle">
+                            <span class="top"></span>
+                            <span class="middle"></span>
+                            <span class="bottom"></span>
+                        </div>
+                    </div>
+                    <div class="overlay" id="overlay">
+                        <nav class="overlay-menu">
+                            <ul>
+                                <li><a href="index.html">Pagina Principal</a></li>
+                                <li><a href="studio.html">La Moda Como Una Industria</a></li>
+                                <li><a href="project-description.html ">La Moda y Los Jovenes</a></li>
+                                <li><a href="projects.html">Lo aesthetic</a></li>
+                                <li><a href="contact.html">Datos del Programador</a></li>
+                                <li><a href="Form.html">Formulario</a></li>
+
+                            </ul>
+                        </nav>
+                    </div>
+                </div>
+    </nav>
+	
+
+     <footer>
+      <div class="container">
+        <div class="row">
+          <div class="col-md-3 col-sm-3">
+          </div>
+          <div class="col-md-6 col-sm6 section-bg-color">
+            <div class="row">
+              <div class="col-sm-9">
+   
+                <h2>INFORMACION DE CONTACTO</h2>
+                <p>Pnumero: 235 107 9429</p>
+                <p>Email: josejoelruiz7@gmail.com</p>
+              </div><!-- /col-sm-9 -->
+            </div><!--/row-->
+          </div><!-- /col-sm-6 -->
+          <div class="col-md-3 col-sm-3 section-bg-color1">
+            <div class="row">
+              <div class="col-sm-9">
+              <h3>Todos los derechos reservados</h3>
+              <p class="pull-right"><a href="https://x.com/Joel_Reys_1989"><img src="images/twitter.png" alt=""/></a></p>
+              <p class="pull-right icons-f"><a href="https://www.facebook.com/josejoel.ruizreyes.7"><img src="images/facebook.png" alt=""/></a></p>
+              </div><!-- /col-sm-9 -->
+            </div><!--/row-->
+          </div><!-- /col-sm-3 -->
+        </div><!-- /row -->
+      </div><!-- /container -->
+    </footer><!-- /footer -->   
+     
+
+    <script src="js/jquery-1.11.0.js"></script>
+  
+	<script src="js/bootstrap.min.js"></script>
+   
+    <script src="js/masonry.js"></script>
+   
+	<script src="js/nav.js"></script>
+    
+    <script src="js/smoothscroll.js"></script>
+    
+    <script src="js/wow.js"></script>
+    
+    <script>
+    new WOW().init();
+    </script>
+
+
+	</body>
+</html>
